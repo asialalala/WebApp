bc955108494e44d8993fe57baaa72e50dbde1d6d
Joasia Klasik <asklas@op.pl>
2024-05-19 12:12:26 +0200

    parser date

--- kongo/src/app/findRooms/findRooms.component.ts
+++ kongo/src/app/findRooms/findRooms.component.ts
@@ -8,6 +8,25 @@ import { Component } from '@angular/core';
 export class FindRoomsComponent {
   startDate: Date = new Date();
   endDate: Date = new Date();
+
+
+  onFind(): void {
+    const dayEnd: number = this.endDate.getDate();
+    const monthEnd: number = this.endDate.getMonth() + 1; // Counting from 1
+    const yearEnd: number = this.endDate.getFullYear();
+
+    const dayStart: number = this.endDate.getDate();
+    const monthStart: number = this.endDate.getMonth() + 1;
+    const yearStart: number = this.endDate.getFullYear();
+
+    // Create dring inf format"RRRR-MM-DD"
+    const formattedEndDate: string = `${yearEnd}-${monthEnd.toString().padStart(2, '0')}-${dayEnd.toString().padStart(2, '0')}`;
+    const formattedStartDate: string = `${yearStart}-${monthStart.toString().padStart(2, '0')}-${dayStart.toString().padStart(2, '0')}`;
+    
+    console.log(formattedEndDate);
+    console.log(formattedStartDate);
+
+  }
 }
 
 interface Room {
