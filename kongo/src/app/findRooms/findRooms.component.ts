4683c799ad4c21424fc8b6492d0f07f2972eb09f
Joasia Klasik <asklas@op.pl>
2024-05-25 11:27:46 +0200

    Display avaliable rooms

--- kongo/src/app/findRooms/findRooms.component.ts
+++ kongo/src/app/findRooms/findRooms.component.ts
@@ -10,34 +10,42 @@ export class FindRoomsComponent {
   startDate: Date = new Date();
   endDate: Date = new Date();
   rooms: Room[] = [];
+  msg: string = "";
+
   constructor(private http: HttpClient) { }
 
-
-  // TO DO verify if start date < end date
   onFind(): void {
+    this.msg = ""
+    this.rooms = [];
+    if (this.startDate < this.endDate) {
+      const dayStart: number = this.startDate.getDate();
+      const monthStart: number = this.startDate.getMonth() + 1;
+      const yearStart: number = this.startDate.getFullYear();
 
-    const dayStart: number = this.startDate.getDate();
-    const monthStart: number = this.startDate.getMonth() + 1;
-    const yearStart: number = this.startDate.getFullYear();
+      const dayEnd: number = this.endDate.getDate();
+      const monthEnd: number = this.endDate.getMonth() + 1; // Counting from 1
+      const yearEnd: number = this.endDate.getFullYear();
 
-    const dayEnd: number = this.endDate.getDate();
-    const monthEnd: number = this.endDate.getMonth() + 1; // Counting from 1
-    const yearEnd: number = this.endDate.getFullYear();
+      // Create string info format"RRRR-MM-DD"
+      const formattedStartDate: string = `${yearStart}-${monthStart.toString().padStart(2, '0')}-${dayStart.toString().padStart(2, '0')}`;
+      const formattedEndDate: string = `${yearEnd}-${monthEnd.toString().padStart(2, '0')}-${dayEnd.toString().padStart(2, '0')}`;
 
-    // Create dring inf format"RRRR-MM-DD"
-    const formattedStartDate: string = `${yearStart}-${monthStart.toString().padStart(2, '0')}-${dayStart.toString().padStart(2, '0')}`;
-    const formattedEndDate: string = `${yearEnd}-${monthEnd.toString().padStart(2, '0')}-${dayEnd.toString().padStart(2, '0')}`;
-  
-    
-    console.log(formattedEndDate);
-    console.log(formattedStartDate);
 
-    this.getItems(formattedStartDate, formattedEndDate);
-  
+      console.log(formattedEndDate);
+      console.log(formattedStartDate);
+
+      this.getItems(formattedStartDate, formattedEndDate);
+    }
+    else
+    {
+      this.msg = "The start date can't be ";
+    }
+
+
   }
 
   getItems(start: string, end: string): void {
-    console.log("Tying to connect!");
+    console.log("Trying to connect!");
     const params = new HttpParams().set('startDate', start).set('endDate', end);
     this.http.get<Room[]>('/api/find', { params }).subscribe({
       next: (response) => {
